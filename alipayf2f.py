b873153b2da3175778a12d3b0f7a4525b74b3e16
wangates <mickeyking98@126.com>
2017-11-06 08:02:52 +0800

    Update alipayf2f.py

--- alipayf2f.py
+++ alipayf2f.py
@@ -2,111 +2,212 @@ package Demo2_3;
 
 import java.util.Scanner;
 import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by hj on 16-7-14.
- * 使用向量保存图书信息，包括书名、作者、出版社、刊号、出版日期、页数和摘要，
- * 并能够实现图书的插入、删除、查找功能。插入、删除时要显示操作是否正确与否的提示信息；
- * 查找时按关键字值进行查找，并显示查找结果。
+ * Created by hj on 16-7-19.
+ * 设计一个简单的学生成绩管理系统，
+ * 要求能够录入并存储学生信息、课程信息以及每个学生每门课程的成绩。
+ * 学生信息包括姓名、学号、性别和班级，课程信息包括课程号、课程名和学分。
+ *
+ * 系统的功能包括：
+ *          计算每个学生的总成绩、
+ *            平均成绩和总学分，
+ *          计算每个班级的平均成绩，
+ *          按照学生平均成绩由高到低的顺序进行排序，
+ *          统计所有学生每门课程的平均成绩。
  */
-class Demo_2_3_3 {
-    private Book_Info tmp;
-    private Vector<Book_Info> BookVector;
-    private Scanner in = new Scanner(System.in);
+class Demo_2_3_4 {
 
-    Demo_2_3_3()
+    private Vector<Stu_Info> StuVector;
+
+    Demo_2_3_4()
     {
         /**
-         * 构造函数
+         构造函数
          */
-        BookVector = new Vector<>(20, 1);
-        System.out.println("请输入初始化时图书信息数目");
-        int booknum = in.nextInt();
+        StuVector = new Vector<>(10,1);
+        System.out.println("请输入初始化是需要输入的学生数目：");
+        Scanner in = new Scanner(System.in);
+        int stusNum = in.nextInt();
         int i;
-        for(i=0;i<booknum;i++)
-            this.insert();
-    }
-
-    private void insert()
-    {
-        /**
-         * 插入
-         */
-        tmp = new Book_Info();
-        BookVector.addElement(tmp);
-    }
-
-    private void selete()
-    {
-        /**
-         * 查询
-         */
-        System.out.println("请输入要查询的书目信息(模糊搜索)：");
-        String search_info = in.next();
-        int VectorSize = BookVector.size();
-        int i;
-        for(i=0;i<VectorSize;i++)
+        for(i=0;i<stusNum;i++)
         {
-            //从Vector中取出的是Object类型变量，需要转换成所需类型
-            tmp = BookVector.elementAt(i);
-            if (this.infoContain(tmp,search_info))
-                tmp.displayInfo();
+            Stu_Info tmp = new Stu_Info();
+            StuVector.addElement(tmp);
         }
     }
 
-    private boolean infoContain(Book_Info book, String search) {
-        return book.bookname.contains(search) ||
-                book.author.contains(search) ||
-                book.press.contains(search) ||
-                book.ISBN.contains(search) ||
-                book.publishData.contains(search) ||
-                book.pageNum.contains(search) ||
-                book.bookAbstrac.contains(search);
-    }
-
-    void delete()
+    void show_averagGrade()
     {
         /**
-         * 删除
+         展示平均成绩
          */
-        System.out.println("请输入要删除书籍的ISBN号（刊号）。");
-        String ISBN = in.next();
-        boolean opreationFlag = true;
-
-        //=============可以重构的部分===============
-        int VectorSize = BookVector.size();
+        int StusNum = StuVector.size();
         int i;
-        for(i=0;i<VectorSize;i++)
+        Stu_Info tmp;
+        for(i=0;i<StusNum;++i)
         {
-            //从Vector中取出的是Object类型变量，需要转换成所需类型
-            tmp = BookVector.elementAt(i);
-            if(tmp.ISBN.contains(ISBN))
-            {
-                opreationFlag = false;
-                System.out.println("您要删除的书籍信息为：");
-                tmp.displayInfo();
+            tmp = StuVector.elementAt(i);
+            System.out.println("姓名："+tmp.stuNum+"\t平均成绩(加权)："+tmp.averagGrade);
+        }
+    }
 
-                System.out.println("确认删除吗：？（Y/N）");
-                String answer = in.next();
-                switch (answer) {
-                    case "Y":
-                    case "y":
-                        BookVector.remove(i);
-                        break;
-                    case "N":
-                    case "n":
-                        System.out.println("取消删除");
-                        break;
-                    default:
-                        System.out.println("输入了错误的信息，请重新操作。");
-                        break;
+    void show_totalCredit()
+    {
+        /**
+         展示总学分
+         */
+        int StusNum = StuVector.size();
+        int i;
+        Stu_Info tmp;
+        for(i=0;i<StusNum;++i)
+        {
+            tmp = StuVector.elementAt(i);
+            System.out.println("姓名："+tmp.stuNum+"\t总学分："+tmp.totalCredit);
+        }
+    }
+
+    void show_totalGrade()
+    {
+        /**
+         展示总成绩
+         */
+        int StusNum = StuVector.size();
+        int i;
+        Stu_Info tmp;
+        for(i=0;i<StusNum;++i)
+        {
+            tmp = StuVector.elementAt(i);
+            System.out.println("姓名："+tmp.stuNum+"\t总成绩： "+tmp.totalGrade);
+        }
+    }
+
+    void getClassAveragGrade(String classNum)
+    {
+        /**
+         @parm 班号
+         展示班级平均成绩
+         */
+
+        int stusNum = StuVector.size();
+        int i;
+        double classStuNum = 0,classToallGrade = 0,classAveragGrade;
+        Stu_Info tmp;
+        for(i=0;i<stusNum;++i)
+        {
+            tmp = StuVector.elementAt(i);
+            if (tmp.classNum.equals(classNum))
+            {
+                classToallGrade += tmp.averagGrade;
+                classStuNum += 1;
+            }
+        }
+        if (classStuNum == 0)
+        {
+            System.out.println("未查询此班号");
+            return ;
+        }
+        classAveragGrade = classToallGrade/classStuNum;
+        System.out.println("班级"+classNum+"的平均成绩: "+classAveragGrade);
+    }
+
+    void getCoursesGrade(String coursesNum)
+    {
+        /**
+         @parm 课序号
+         展示班级平均成绩
+         */
+
+        int stusNum = StuVector.size(),courNUM;
+        int i,j;
+        double coursesStuNum = 0,coursesToallGrade = 0,coursesAveragGrade = 0;
+        Stu_Info tmp;
+        Courses_Info tmp2;
+        for(i=0;i<stusNum;++i)
+        {
+            tmp = StuVector.elementAt(i);
+            courNUM = tmp.coursesVector.size();
+            for(j=0;j<courNUM;++j)
+            {
+                tmp2 = tmp.coursesVector.elementAt(j);
+                if(tmp2.coursesNum.equals(coursesNum))
+                {
+                    coursesToallGrade +=  tmp2.grade;
+                    coursesStuNum += 1;
+                    break;
                 }
             }
         }
-        if(opreationFlag)
-            System.out.println("未查询到对应的书籍信息");
-
+        if (coursesStuNum == 0)
+        {
+            System.out.println("未查询此课序号");
+            return ;
+        }
+        coursesAveragGrade = coursesToallGrade/coursesStuNum;
+        System.out.println("课程"+coursesNum+"的平均成绩: "+coursesAveragGrade);
     }
 
+    private List<String> getAllCoursesNum()
+    {
+        /**
+         @Return 所有课序号构成的容器
+         获取不重复的所有课序号
+         */
+        List<String> coursList = new ArrayList<>();
+        //Vector<String> coursVector = new Vector<>(10,5);
+        int stusNum = StuVector.size() , courNUM;
+        int i,j;
+        Stu_Info tmp;
+        Courses_Info tmp2;
+        for(i=0;i<stusNum;++i)
+        {
+            tmp = StuVector.elementAt(i);
+            courNUM = tmp.coursesVector.size();
+            for(j=0;j<courNUM;++j)
+            {
+                tmp2 = tmp.coursesVector.elementAt(j);
+                //添加不重复的课程列表
+                if (!coursList.contains(tmp2.coursesNum))
+                    coursList.add(tmp2.coursesNum);
+            }
+        }
+       return coursList;
+    }
+
+    void show_all_courses_averageGrade()
+    {
+        List<String> coursList = new ArrayList<>();
+        coursList = getAllCoursesNum();
+        int size = coursList.size();
+        int i;
+        for(i=0;i<size;++i)
+        {
+            getCoursesGrade(coursList.get(i));
+        }
+    }
+
+    void sort_by_averaneGrade()
+    {
+        int vectorSize = StuVector.size();
+        int i,j;
+        Stu_Info tmp;
+        //简单冒泡排序
+        for(i=0;i<vectorSize-1;++i)
+        {
+            for(j=0;j<vectorSize-1;++j)
+            {
+                if ((StuVector.elementAt(j).averagGrade<StuVector.elementAt(j+1).averagGrade))
+                {
+                    tmp = StuVector.elementAt(j);
+                    StuVector.setElementAt(StuVector.elementAt(j+1),j);
+                    StuVector.setElementAt(tmp,j+1);
+                }
+            }
+        }
+        System.out.println("按平均成绩排序如下：");
+        show_averagGrade();
+    }
 
 }
